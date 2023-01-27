7a0163eefb47498d3b74fbbecb8ecf5429146050
waaaw <waaaw@naver.com>
2023-01-28 02:07:10 +0900

    Pagination(1. AppBar와 TabBar만들기

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,17 +3,43 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final Widget? bottomNavigationBar;
+  final String? title;
 
   const DefaultLayout({
     required this.child,
+    this.title,
     this.backgroundColor,
+    this.bottomNavigationBar,
     Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar(){
+    if(title == null){
+      return null;
+    }else{
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          title!,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
